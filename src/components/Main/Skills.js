aead1489e5a0cd99efc28ba60bf25a5bf77a2d45
Mike Boring <shekinah999@msn.com>
2021-04-12 14:10:17 -0600

    Updated Skills

--- src/components/Main/Skills.js
+++ src/components/Main/Skills.js
@@ -12,29 +12,29 @@ const Skills = () => {
   }
     
   const skills = [
-    {type: "Photoshop", level: 95},
-    {type: "Illustrator", level: 85},
+    {type: "Photoshop", level: 75},
+    {type: "Illustrator", level: 65},
     {type: "Figma", level: 50},
   ];
   const skills2 = [
     {type: "Html", level: 90},
     {type: "Wordpress", level: 80},
-    {type: "Javascript", level: 75},
-    {type: "React.js", level: 75},
-    {type: "Django", level: 75},
+    {type: "Javascript", level: 65},
+    {type: "React.js", level: 60},
+    {type: "Django", level: 60},
     {type: "Css", level: 75},
-    {type: "Redux", level: 75},
+    {type: "Redux", level: 60},
     {type: "View.js", level: 50},
     {type: "TypeScript", level: 50},
   ];
   const skills3 = [
-    {type: "Django", level: 75},
-    {type: "Python", level: 70},
-    {type: "Node.js", level: 70},
-    {type: "Express.js", level: 70},
-    {type: "MongoDB", level: 65},
-    {type: "MySQL", level: 65},
-    {type: "PostgreSQL", level: 60},
+    {type: "Django", level: 60},
+    {type: "Python", level: 60},
+    {type: "Node.js", level: 55},
+    {type: "Express.js", level: 50},
+    {type: "MongoDB", level: 50},
+    {type: "MySQL", level: 50},
+    {type: "PostgreSQL", level: 50},
   ];
 
   return (
