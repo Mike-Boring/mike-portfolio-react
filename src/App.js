55685665a7953fc45b126353278007b3748d44e8
Mike Boring <shekinah999@msn.com>
2022-07-09 18:15:03 -0600

    Updated redirect

--- src/App.js
+++ src/App.js
@@ -12,22 +12,10 @@ import AnimatedBanner from './components/Header/Banner.js'
 import Contact from './components/Main/Contact.js'
 
 
-function detec() { 
-    if (navigator.userAgent.match(/Android/i) 
-        || navigator.userAgent.match(/webOS/i) 
-        || navigator.userAgent.match(/iPhone/i)  
-        || navigator.userAgent.match(/iPad/i)  
-        || navigator.userAgent.match(/iPod/i) 
-        || navigator.userAgent.match(/BlackBerry/i) 
-        || navigator.userAgent.match(/Windows Phone/i)) { 
-        window.location = "https://mike-boring.github.io/mike-portfolio-react/" 
-    } else { 
-        return
-    } 
-}
+
 
 function App() {
-    detec()
+    
     const { isSticky1, element1 } = useSticky1()
     return (
         <>
