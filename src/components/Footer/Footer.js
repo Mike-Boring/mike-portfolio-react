99e32eb486896101355a77cc28a5b6567ee23349
Mike Boring <shekinah999@msn.com>
2022-07-09 19:12:49 -0600

    Adjusted Footer tag

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,12 @@ import ScrollTopArrow from '../../components/Main/ScrollTopArrow.js';
 const Footer = () => {
     let currentYear = new Date().getFullYear()
     return (
-        <section className='footer'>
+        <footer className='footer'>
             <div className="footerContent">
             <h6>© {currentYear} Mike Boring - Full Stack Software Engineer</h6>
             </div>
             <ScrollTopArrow />
-        </section>
+        </footer>
     )
 };
 
